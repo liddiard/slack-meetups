6565f480a652ffdf3bdf05cbe0e2e2375d2ae1e0
Harrison Liddiard <hliddiard@paypal.com>
2019-05-23 23:04:47 -0700

    get app debug setting from env var

--- meetups/settings.py
+++ meetups/settings.py
@@ -25,7 +25,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if "DEBUG" in os.environ:
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = [
     "slack-meetups.appspot.com"
