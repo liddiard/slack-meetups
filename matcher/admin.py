66277bad34add868f071d38619ac17eaa0132395
Harrison Liddiard <hliddiard@paypal.com>
2020-03-27 12:08:44 -0700

    fix incorrect import

--- matcher/admin.py
+++ matcher/admin.py
@@ -7,7 +7,7 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
-from django.contrib.auth.models import GroupAdmin
+from django.contrib.auth.admin import GroupAdmin
 from django.http import HttpResponse
 
 from .models import Pool, Person, Round, Match
