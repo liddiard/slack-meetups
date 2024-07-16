8cbbcb4bb0c6dc212fd27a604bb8c19d698475b0
Harrison Liddiard <omniaura5@gmail.com>
2024-07-16 09:08:50 -0400

    remove unnecessary .env file parsing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 # Docker script to set up and run the Django app
 
-# Load environment variables from .env file, stripping comments
-if [ -f .env ]; then
-  export $(cat .env | grep -v '^#' | xargs)
-fi
-
 # Run Django management commands
 python manage.py collectstatic --noinput
 python manage.py makemigrations matcher
